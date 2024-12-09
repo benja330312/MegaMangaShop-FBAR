3f5e4871408af7700e2e05e9c4c83f7e8efdce82
Benjamin FOUILHAC <bfouilhac@gmail.com>
2024-12-09 16:26:28 +0100

    - Ajout d'asserts pour vérifier que le montant HT est valide (sup à 0)

--- src/main/kotlin/montaine/guillaume/TicketDeCaisse.kt
+++ src/main/kotlin/montaine/guillaume/TicketDeCaisse.kt
@@ -4,6 +4,15 @@ import de.huxhorn.sulky.ulid.ULID
 
 class TicketDeCaisse(val Id: String = ULID().nextULID(), val boutique: Boutique, val TotalHT: Double) {
 
+    init {
+        if (TotalHT.equals(0)) {
+            throw IllegalArgumentException("Le montant HT ne peut pas être nul ou inférieur ou égal à zéro.")
+        }
+        if (TotalHT < 0){
+            throw IllegalArgumentException("Le montant HT est nul !")
+        }
+    }
+
     fun getTax(): Double {
         return boutique.Pays.getTVA()
     }
@@ -20,7 +29,7 @@ class TicketDeCaisse(val Id: String = ULID().nextULID(), val boutique: Boutique,
     }
 
     fun getTotalTTC(): Double {
-        return TotalHT * getRemise() * getTax()
+        return TotalHT * (1 - getRemise()) * (1 + getTax()/100.0)
     }
 
     fun appliquerRemise(): Double {
