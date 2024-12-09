76f21a72bf554c6a65ccb0bca67a6dab1681acff
Benjamin FOUILHAC <bfouilhac@gmail.com>
2024-12-09 15:11:59 +0100

    - Ajout d'une méthode qui donne la réduction en se basant sur le montant HT - Ajout d'une méthode qui donne la TVA en se basant sur le pays d'origine - Ajout d'une méthode qui donne le total TTC.

--- src/main/kotlin/montaine/guillaume/TicketDeCaisse.kt
+++ src/main/kotlin/montaine/guillaume/TicketDeCaisse.kt
@@ -2,21 +2,24 @@ package montaine.guillaume
 
 import de.huxhorn.sulky.ulid.ULID
 
-class TicketDeCaisse(val Id: String = ULID().nextULID(), val boutique: Boutique) {
-
-    val TotalHT: Double = 0.0
-    val Taxe: Double = 0.0
-    val Remise: Double = 0.0
-
-    fun getTotalHT(): Double {
-        return TotalHT
-    }
+class TicketDeCaisse(val Id: String = ULID().nextULID(), val boutique: Boutique, val TotalHT: Double) {
 
     fun getTax(): Double {
-        return TotalHT
+        return boutique.Pays.getTVA()
     }
 
     fun getRemise(): Double {
-        return Remise
+        return when {
+            TotalHT > 1000 -> 0.10
+            TotalHT in 500.0..1000.0 -> 0.07
+            TotalHT in 300.0..500.0 -> 0.05
+            TotalHT in 200.0..300.0 -> 0.03
+            TotalHT in 150.0..200.0 -> 0.02
+            else -> 0.0
+        }
+    }
+
+    fun getTotalTTC(): Double {
+        return TotalHT * getRemise() * getTax()
     }
 }
